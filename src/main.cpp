e8a5529fb8cb30dbd5617e3f1d55c824c82bc478
disrado <n.maltsev.dis@gmail.com>
2018-06-10 17:39:16 +0300

    Tempate function for hashing

--- src/main.cpp
+++ src/main.cpp
@@ -10,61 +10,18 @@
 #include <hex.h>
 #include <config.h>
 
-void md5(const std::string& sourceString, std::string& hashHolder)
-{
+
 //	CryptoPP::MD5 sha1;
 //	CryptoPP::StringSource("nikita", true, new CryptoPP::HashFilter(sha1, new CryptoPP::HexEncoder(new CryptoPP::StringSink(hashHolder))));
 
-    CryptoPP::MD5 md5;
-    CryptoPP::byte digest[CryptoPP::MD5::DIGESTSIZE];
 
-    md5.CalculateDigest(digest, (CryptoPP::byte*)sourceString.c_str(), sourceString.length());
-
-    CryptoPP::HexEncoder encoder;
-
-    encoder.Attach(new CryptoPP::StringSink(hashHolder));
-    encoder.Put(digest, sizeof(digest));
-    encoder.MessageEnd();
-
-}
-
-void sha1(const std::string& sourceString, std::string& hashHolder)
+template<typename T>
+void getHash(const std::string& sourceString, std::string& hashHolder, T& algorithm)
 {
-//	CryptoPP::SHA1 sha1;
-//	CryptoPP::StringSource(sourceString, true, new CryptoPP::HashFilter(sha1, new CryptoPP::HexEncoder(new CryptoPP::StringSink(hashHolder))));
+    CryptoPP::byte digest[T::DIGESTSIZE];
 
-    CryptoPP::SHA1 md5;
-    CryptoPP::byte digest[CryptoPP::SHA1::DIGESTSIZE];
 
-    md5.CalculateDigest(digest, (CryptoPP::byte*)sourceString.c_str(), sourceString.length());
-
-    CryptoPP::HexEncoder encoder;
-
-    encoder.Attach(new CryptoPP::StringSink(hashHolder));
-    encoder.Put(digest, sizeof(digest));
-    encoder.MessageEnd();
-}
-
-void sha256(const std::string& sourceString, std::string& hashHolder)
-{
-    CryptoPP::SHA256 md5;
-    CryptoPP::byte digest[CryptoPP::SHA256::DIGESTSIZE];
-
-    md5.CalculateDigest(digest, (CryptoPP::byte*)sourceString.c_str(), sourceString.length());
-
-    CryptoPP::HexEncoder encoder;
-
-    encoder.Attach(new CryptoPP::StringSink(hashHolder));
-    encoder.Put(digest, sizeof(digest));
-    encoder.MessageEnd();
-}
-
-void sha224(const std::string& sourceString, std::string& hashHolder)
-{
-    CryptoPP::SHA224 md5;
-    CryptoPP::byte digest[CryptoPP::SHA224::DIGESTSIZE];
-
-    md5.CalculateDigest(digest, (CryptoPP::byte*)sourceString.c_str(), sourceString.length());
+    algorithm.CalculateDigest(digest, (CryptoPP::byte*)sourceString.c_str(), sourceString.length());
 
     CryptoPP::HexEncoder encoder;
 
@@ -74,13 +31,13 @@ void sha224(const std::string& sourceString, std::string& hashHolder)
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
     std::cout << "Enter file name: ";
 
     std::string fileName;
     std::cin >> fileName;
-        
+
     std::ifstream file(fileName.c_str());
 
     std::stringstream buffer;
@@ -88,25 +45,9 @@ int main()
     
     std::string hash = "";
 
-    md5(buffer.str(), hash);
+    CryptoPP::MD5 algo;
 
-    std::cout << "MD5: " << hash << std::endl;
+    getHash(buffer.str(), hash, algo);
 
-    hash = "";
-
-    sha1(buffer.str(), hash);
-
-    std::cout << "SHA1: " << hash << std::endl;
-    
-    hash = "";
-
-    sha256(buffer.str(), hash);
-
-    std::cout << "SHA256: " << hash << std::endl;
-
-    hash = "";
-
-    sha224(buffer.str(), hash);
-
-    std::cout << "SHA224: " << hash << std::endl;
+    std::cout << "hash: " << hash << std::endl;
 }
